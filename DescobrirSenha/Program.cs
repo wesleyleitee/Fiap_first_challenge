a1fe70f37b31b3ed47cd0bcb9ada9eabf5c4a6c1
Wesley Leite <leitewesleyleite@gmail.com>
2024-09-01 18:34:09 -0300

    Better code

--- DescobrirSenha/Program.cs
+++ DescobrirSenha/Program.cs
@@ -1,5 +1,9 @@
-﻿using System.Text;
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
 using System.Text.Json;
+using System.Threading.Tasks;
 
 class Program
 {
@@ -7,29 +11,41 @@ class Program
     {
         var httpClient = new HttpClient();
         string url = "https://fiapnet.azurewebsites.net/fiap";
-        string grupo = "12"; 
+        string group = "12";  // Replace with your group
 
-        char[] capital_letters  = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".ToCharArray();
-        char[] lowercase_letters = "abcdefghijklmnopqrstuvwxyz".ToCharArray();
+        char[] uppercaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".ToCharArray();
+        char[] lowercaseLetters = "abcdefghijklmnopqrstuvwxyz".ToCharArray();
         char[] numbers = "0123456789".ToCharArray();
 
         // Parallel.For to try different password combinations in parallel
-        Parallel.For(0, capital_letters.Length, (i, state) =>
+        Parallel.For(0, uppercaseLetters.Length, (i, state) =>
         {
-            for (int j = 0; j < lowercase_letters.Length; j++)
+            for (int j = 0; j < lowercaseLetters.Length; j++)
             {
                 for (int k = 0; k < numbers.Length; k++)
                 {
                     for (int l = 0; l < numbers.Length; l++)
                     {
-                        string password = $"{capital_letters[i]}{lowercase_letters[j]}{numbers[k]}{numbers[l]}";
-                        var response = TryPassword(httpClient, url, grupo, password).Result;
+                        // Generate a combination of characters
+                        char[] passwordArray = { uppercaseLetters[i], lowercaseLetters[j], numbers[k], numbers[l] };
 
-                        if (response)
+                        // Generate all possible permutations of this combination
+                        var permutations = GetPermutations(passwordArray);
+
+                        foreach (var password in permutations)
                         {
-                            Console.WriteLine($"Password found: {password}");
-                            state.Stop();  // for all threads
-                            return;
+                            var response = TryPassword(httpClient, url, group, password).Result;
+
+                            if (response)
+                            {
+                                Console.WriteLine($"Password found: {password} - Success");
+                                state.Stop();  // Stop all threads
+                                return;
+                            }
+                            else
+                            {
+                                Console.WriteLine($"Password: {password} - Failed");
+                            }
                         }
                     }
                 }
@@ -40,10 +56,42 @@ class Program
         Console.WriteLine("Process finished.");
     }
 
-    static async Task<bool> TryPassword(HttpClient httpClient, string url, string grupo, string password)
+    // Generate all permutations of a character array
+    static IEnumerable<string> GetPermutations(char[] array)
     {
-        var data = new { Key = password, grupo };
+        var results = new List<string>();
+        Permute(array, 0, array.Length - 1, results);
+        return results;
+    }
 
+    // Helper function to generate permutations
+    static void Permute(char[] array, int l, int r, List<string> results)
+    {
+        if (l == r)
+        {
+            results.Add(new string(array));
+        }
+        else
+        {
+            for (int i = l; i <= r; i++)
+            {
+                Swap(ref array[l], ref array[i]);
+                Permute(array, l + 1, r, results);
+                Swap(ref array[l], ref array[i]); // backtrack
+            }
+        }
+    }
+
+    static void Swap(ref char a, ref char b)
+    {
+        char temp = a;
+        a = b;
+        b = temp;
+    }
+
+    static async Task<bool> TryPassword(HttpClient httpClient, string url, string group, string password)
+    {
+        var data = new { Key = password, group = group };
         var jsonContent = new StringContent(JsonSerializer.Serialize(data), Encoding.UTF8, "application/json");
 
         try
@@ -51,15 +99,14 @@ class Program
             var response = await httpClient.PostAsync(url, jsonContent);
             if (response.IsSuccessStatusCode)
             {
-                // Here you can check if the password was correct depending on the expected answer
+                // Here you can check if the password was correct, depending on the expected response
                 string responseBody = await response.Content.ReadAsStringAsync();
-                Console.WriteLine($"Password {password} - {response.StatusCode} {responseBody}");
-                return responseBody.Contains("Correct password");  // Adjust according to the correct answer
+                return responseBody.Contains("correct password");  // Adjust according to the correct response
             }
         }
         catch (Exception ex)
         {
-            Console.WriteLine($"Error when trying password {password}: {ex.Message}");
+            Console.WriteLine($"Error trying password {password}: {ex.Message}");
         }
 
         return false;
